d46e103f9bc96be973cb6e46dabe9947eb1444d9
weilin <1404102344@qq.com>
2020-09-13 18:48:08 +0800

    完成注册登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,5 @@
-$(function () {
-    $('#link_reg').on('click', function () {
-        $('.login-box').hide().siblings().show()
-    });
-    $('#link_login').on('click', function () {
-        $('.reg-box').hide().siblings().show()
-    })
-})
 var form = layui.form;
+var layer = layui.layer;
 form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -21,4 +14,49 @@ form.verify({
             return '两次密码不一致！'
         }
     }
+
+})
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide().siblings().show()
+    });
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide().siblings().show()
+    })
+    // 登录
+    $('#login-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败!')
+                }
+                layer.msg('登录成功!')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+    // 注册
+    $('#reg-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+
+                $('#link_login').click()
+                layer.msg('注册成功,请登录')
+            }
+        })
+    })
 })
